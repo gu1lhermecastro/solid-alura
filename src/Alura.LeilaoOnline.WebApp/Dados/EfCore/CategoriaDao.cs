019c9afe2db9899bc290d10575649289109ad6cd
Guilherme Castro <guilherme.castro@aditum.com.br>
2020-08-09 14:52:40 -0300

    Adiciona CategoriaDao

---
+++ src/Alura.LeilaoOnline.WebApp/Dados/EfCore/CategoriaDao.cs
@@ -0,0 +1,41 @@
+﻿using Alura.LeilaoOnline.WebApp.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Alura.LeilaoOnline.WebApp.Dados.EfCore
+{
+    public class CategoriaDao : ICategoriaDao
+    {
+        AppDbContext _context;
+
+        public CategoriaDao(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Alterar(Categoria obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Categoria BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Categoria> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(Categoria obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Incluir(Categoria obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
